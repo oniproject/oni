b911aa83de89e8ee2504485d58f37aa2841bef41
Lain-dono <lain.dono@gmail.com>
2018-08-13 08:23:10 +0300

    fix rooms

--- rooms/src/replica.rs
+++ rooms/src/replica.rs
@@ -65,37 +65,3 @@ impl<S: Shim> std::iter::Extend<Index> for Replica<S> {
         self.removed.sort();
     }
 }
-
-#[test]
-fn calc() {
-    let range = View::Within(10.0);
-    let mut replica = Replica::<f32>::new(range);
-
-    {
-        replica.extend(vec![1, 2, 3]);
-        assert_eq!(&replica.created(), &[1, 2, 3]);
-        assert_eq!(&replica.removed(), &[]);
-        assert_eq!(&replica.nchange(), &[]);
-    }
-
-    {
-        replica.extend(vec![4, 2, 3, 4]);
-        assert_eq!(&replica.created(), &[4]);
-        assert_eq!(&replica.removed(), &[1]);
-        assert_eq!(&replica.nchange(), &[2, 3]);
-    }
-
-    {
-        replica.extend(vec![4, 2, 3, 4]);
-        assert_eq!(&replica.created(), &[]);
-        assert_eq!(&replica.removed(), &[]);
-        assert_eq!(&replica.nchange(), &[2, 3, 4]);
-    }
-
-    {
-        replica.extend(std::iter::empty());
-        assert_eq!(&replica.created(), &[]);
-        assert_eq!(&replica.removed(), &[2, 3, 4]);
-        assert_eq!(&replica.nchange(), &[]);
-    }
-}
